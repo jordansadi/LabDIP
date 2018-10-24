b1d28eb2fa8e9e341f929896509ce69c06864b6c
jordan <jsadi@my.wctc.edu>
2018-10-23 19:22:02 -0500

    lab2 completed

---
+++ src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
@@ -0,0 +1,56 @@
+package dip.lab2.student.solution1;
+
+import javafx.concurrent.Service;
+
+public class FoodServiceTipCalculator implements TipCalculator {
+    private static final double MIN_BILL = 0.00;
+    private static final String BILL_ENTRY_ERR =
+            "Error: bill must be greater than or equal to " + MIN_BILL;
+    private static final double GOOD_RATE = 0.20;
+    private static final double FAIR_RATE = 0.15;
+    private static final double POOR_RATE = 0.10;
+
+    private double bill;
+    private ServiceQuality serviceQuality;
+
+    public FoodServiceTipCalculator(ServiceQuality q, double billAmt) {
+        this.setServiceRating(q);
+        this.setBill(billAmt);
+    }
+
+    @Override
+    public double getTip() {
+        double tip = 0.00; // always initialize local variables
+
+        switch(serviceQuality) {
+            case GOOD:
+                tip = bill * GOOD_RATE;
+                break;
+            case FAIR:
+                tip = bill * FAIR_RATE;
+                break;
+            case POOR:
+                tip = bill * POOR_RATE;
+                break;
+        }
+
+        return tip;
+    }
+
+    @Override
+    public void setServiceRating(ServiceQuality q) {
+        serviceQuality = q;
+    }
+
+    @Override
+    public ServiceQuality getServiceQuality() {
+        return serviceQuality;
+    }
+
+    public final void setBill(double billAmt) {
+        if(billAmt < MIN_BILL) {
+            throw new IllegalArgumentException(BILL_ENTRY_ERR);
+        }
+        bill = billAmt;
+    }
+}
